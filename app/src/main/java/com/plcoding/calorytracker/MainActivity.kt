e9c9f157b757ddd15a74cda061777afca84986d3
Michał Błachut <michal.blachut@microsoft.wsei.edu.pl>
2022-11-25 13:56:45 +0100

    Initial commit

---
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.plcoding.calorytracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.calorytracker.ui.theme.CaloryTrackerTheme
+import com.plcoding.onboarding_presentation.welcome.WelcomeScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CaloryTrackerTheme {
+                WelcomeScreen()
+            }
+        }
+    }
+}
