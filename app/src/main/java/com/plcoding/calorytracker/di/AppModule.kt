26586412cfc5936ec6d37a0303b6a46b595c5c47
Michał Błachut <michal.blachut@microsoft.wsei.edu.pl>
2023-03-13 18:04:13 +0100

    Added TrackerOverviewViewModel.kt along with some drawables

--- app/src/main/java/com/plcoding/calorytracker/di/AppModule.kt
+++ app/src/main/java/com/plcoding/calorytracker/di/AppModule.kt
@@ -3,7 +3,7 @@ package com.plcoding.calorytracker.di
 import android.app.Application
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
-import com.plcoding.core.domain.DefaultPreferences
+import com.plcoding.core.data.preferences.DefaultPreferences
 import com.plcoding.core.domain.preferences.Preferences
 import com.plcoding.core.domain.use_case.FilterOutDigits
 import dagger.Module
